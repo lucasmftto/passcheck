20a1330a8280d8c433424ef59b2fcfcec2514845
Lucas Favaretto <lucasmelofavaretto@hotmail.com>
2021-07-31 00:22:17 -0300

    Iniciando logica da forma mais simples

--- src/main/java/br/com/passcheck/service/PasswordServiceImpl.java
+++ src/main/java/br/com/passcheck/service/PasswordServiceImpl.java
@@ -1,12 +1,26 @@
 package br.com.passcheck.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PasswordServiceImpl implements PasswordService{
 
+    Logger logger = LoggerFactory.getLogger(PasswordServiceImpl.class);
+
     @Override
     public boolean checkPassword(String password) {
-        return false;
+        logger.info("Iniciando Validacao de password");
+        String[] pass = password.split("");
+
+
+        if (password.equals("")){
+            return false;
+        }else if(pass.length < 9){
+            return false;
+        }
+
+        return true;
     }
 }
